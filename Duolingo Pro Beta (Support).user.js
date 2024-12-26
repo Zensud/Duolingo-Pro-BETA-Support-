edbe81af0668d5d42a6b8a5f86649d0f94120f65
XZe <47523343+Zensusy@users.noreply.github.com>
2024-12-26 21:30:04 +0700

    Update Duolingo Pro Beta (Support).user.js

--- Duolingo Pro Beta (Support).user.js
+++ Duolingo Pro Beta (Support).user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Duolingo Pro Beta (Support)
+// @name         Duolingo Pro (Extension)
 // @namespace    http://tampermonkey.net/
 // @version      2024-04-09
-// @description  Fixi the Listening only mode from the original extension.
+// @description  Fix the Listening only mode from the original extension.
 // @author       Zensud
 // @match        https://www.duolingo.com/*
 // @icon         https://imgs.search.brave.com/V03bRmemSGDlrBy5Iq1IdnhmfODnqNAC0L6F7si5F6w/rs:fit:32:32:1/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvY2NjMzk1YmM5/Y2ZhZTFkYTg3ZTlm/NjNhZjZiYjY3M2Yy/NTZmMmUyNDQwYzkx/MTY2MjJjMWRmYWRi/Mjc4NzQxMy93d3cu/ZHVvbGluZ28uY29t/Lw
@@ -12,14 +12,49 @@
 (function() {
     'use strict';
 
-    const listeningPage = "https://www.duolingo.com/practice-hub/listening-practice"; //Listening url
+    const autobutton = "/html/body/div[5]/div[1]/div[2]/div[3]/div/label";
+    const listeningPage = "www.duolingo.com"; //Listening url
+    const practiceHub = "https://duolingo.com/practice-hub/listening-practice"; // Practice hub URL
+    const buttonXPath = "/html/body/div[1]/div[1]/div/div/div[2]/div/div/div/button[1]";
     let buttonClicked = false;
+    let timer;
+
+    //timer function
+    function clickButton() {
+        const button = document.evaluate(buttonXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        if (button) {
+            button.click();
+            console.log("Button clicked!");
+        } else {
+            console.log("Button not found!");
+        }
+    }
+
+    function startTimer() {
+        timer = setTimeout(clickButton, 60000); // Wait for 60 seconds (60000 milliseconds)
+    }
+
+    function resetTimer() {
+        clearTimeout(timer);
+    }
+
+    function handlePageChange() {
+        if (window.location.href === practiceHub) {
+            resetTimer();
+            console.log("Timer reset!");
+        } else if (window.location.href === listeningPage) {
+            startTimer();
+            console.log("Timer started!");
+        }
+    }
+
+    setInterval(handlePageChange, 3000); 
+    handlePageChange();
 
     function navigateTolisteningPage() {
         window.location.href = listeningPage;
     }
 
-    //click Solve all button automaticlly
     function clickButtonOnce() {
         if (!buttonClicked) {
             const button = document.getElementById("solveAllButton");
@@ -30,21 +65,22 @@
         }
     }
 
-    // Check if the current page is the practice-hub
-    if (window.location.href === "https://www.duolingo.com/practice-hub") { 
-        // If on the practice-hub, navigate to the listening page
+    if (window.location.href === practiceHub) {
         navigateTolisteningPage();
     } else if (window.location.href === listeningPage) {
-        // If on the listening, click the button
         clickButtonOnce();
     }
 
-    // Continuously check if the current page is the practice-hub or the listening page, and navigate/click button accordingly
     setInterval(function() {
-        if (window.location.href === "https://www.duolingo.com/practice-hub") { 
+        if (window.location.href === practiceHub) {
             navigateTolisteningPage();
         } else if (window.location.href === listeningPage) {
             clickButtonOnce();
         }
-    }, 5000); // Adjust the interval 
+    }, 5000); 
 })();
+
+
+
+
+
